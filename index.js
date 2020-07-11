7f621f965b7fb71046d5ab1159baa901ed33a1d4
Aarav-Juneja <61629581+Aarav-Juneja@users.noreply.github.com>
2020-07-10 17:44:58 -0700

    Update index.js
    I added service workers for better experience

--- index.js
+++ index.js
@@ -11,3 +11,11 @@ function myFunction() {
 function search() {
   document.getElementById('search1').style.display = "block"
 }
+
+// Check that service workers are supported
+if ('serviceWorker' in navigator) {
+  // Use the window load event to keep the page load performant
+  window.addEventListener('load', () => {
+    navigator.serviceWorker.register('/service-worker.js');
+  });
+}
