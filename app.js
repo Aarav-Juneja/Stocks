e4bc05ba7da6a542ae569e696d337aab350ae3e1
Aarav-Juneja <61629581+Aarav-Juneja@users.noreply.github.com>
2021-01-24 14:52:01 -0800

    Start node.js
    and ejs

---
+++ app.js
@@ -0,0 +1,13 @@
+const express = require('express');
+const app = express()
+app.use(express.urlencoded({ extended: true }))
+app.use(express.static("public"))
+app.set("view engine", "ejs")
+
+app.get("/", (req, res) => {
+  res.render("home")
+})
+
+app.listen(3000, function() {
+  console.log("Server listening successfully listening.")
+})
